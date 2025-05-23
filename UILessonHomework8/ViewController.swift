2b5ebf39042c27b9e367f6187c27866ad4638ab6
AvazbekNadyrbek <spatium.avazbek@gmail.com>
2025-05-23 14:29:59 +0200

    Initial Commit

---
+++ UILessonHomework8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UILessonHomework8
+//
+//  Created by Авазбек Надырбек уулу on 23.05.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
